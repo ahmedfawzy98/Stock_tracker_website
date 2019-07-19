53e7754ec0864221d418d9da91a7084d5f5f14e7
ahmed <ahmedfawzy671998@gmailcom>
2019-07-19 02:24:04 +0200

    fix errors messages and refoctoring stock_controller

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,9 +1,12 @@
 class Stock < ApplicationRecord
   def self.new_from_lookup(ticker_symbol)
-    StockQuote::Stock.new(api_key: ENV['api_key'])
+    if StockQuote::Stock.api_key.nil?
+      debugger
+      StockQuote::Stock.new(api_key: ENV['api_key'])
+    end
     begin
       stock = StockQuote::Stock.quote(ticker_symbol)
-        new(name: stock.company_name, ticker: stock.symbol,
+      new(name: stock.company_name, ticker: stock.symbol,
             last_price: stock.latest_price)
     rescue Exception => e
       nil
