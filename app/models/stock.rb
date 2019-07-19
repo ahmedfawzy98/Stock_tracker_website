fbf31e72b957f934ef0925d94294272e38a5ab0f
ahmed <ahmedfawzy671998@gmailcom>
2019-07-19 04:23:32 +0200

    many to many associations UserStock model and restrict adding stocks

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,7 +1,13 @@
 class Stock < ApplicationRecord
+  has_many :user_stocks
+  has_many :users, through: :user_stocks
+
+  def self.find_by_ticker(ticker_symbol)
+    where(ticker: ticker_symbol).first
+  end
+
   def self.new_from_lookup(ticker_symbol)
     if StockQuote::Stock.api_key.nil?
-      debugger
       StockQuote::Stock.new(api_key: ENV['api_key'])
     end
     begin
