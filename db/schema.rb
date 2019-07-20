4a21985f8378795ad0e2441b497ffbd9b13e25a4
ahmed <ahmedfawzy671998@gmailcom>
2019-07-20 22:25:34 +0200

    deploy

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_19_184822) do
+ActiveRecord::Schema.define(version: 2019_07_20_200912) do
 
   create_table "friendships", force: :cascade do |t|
     t.integer "user_id"
